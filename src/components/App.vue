908308763c0566ec499233469d414719041688ba
Adam Orlowski <adamorlowskipoland@icloud.com>
2017-12-17 00:08:34 +0100

    add joke component display jokes

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,7 +1,28 @@
 <template>
   <div id="app">
-    Got jokes?
-
-    {{ $store.state }}
+    <h3>
+      Got jokes?
+    </h3>
+    <button class="btn btn-primary"
+            @click="initJokes">
+      Add ten random Jokes
+    </button>
+    <div class="col-md-12">
+      <joke-block v-for="(joke, index) in $store.state.jokes" :joke="joke" key="joke"></joke-block>
+    </div>
   </div>
 </template>
+
+<script>
+  import JokeBlock from './Joke.vue'
+  import { mapActions } from 'vuex'
+
+  export default {
+    components: {
+      JokeBlock
+    },
+    methods: mapActions([
+      'initJokes'
+    ])
+  }
+</script>
