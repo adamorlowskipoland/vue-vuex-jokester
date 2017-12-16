e6fd646ca8acbcf6ad4907781c40929940427a12
Adam Orlowski <adamorlowskipoland@icloud.com>
2017-12-17 00:41:46 +0100

    add filter methods

--- src/components/App.vue
+++ src/components/App.vue
@@ -11,8 +11,18 @@
             @click="addJoke">
       Add random Joke
     </button>
+    <div>
+      <span v-for="type in types">
+        <input type="checkbox"
+               :value="type"
+               v-model="checkedTypes"
+               checked>
+        <label>{{ type }}</label>&nbsp;
+      </span>
+    </div>
     <div class="col-md-12">
       <joke-block v-for="(joke, index) in $store.state.jokes"
+                  v-show="checkedTypes.includes(joke.type)"
                   :joke="joke"
                   :index="index"
                   key="joke">
@@ -26,6 +36,17 @@
   import { mapActions } from 'vuex'
 
   export default {
+    data () {
+      return {
+        types: [
+          'general', 'knock-knock', 'programming'
+        ],
+        checkedTypes: [
+          'general', 'knock-knock', 'programming'
+        ]
+
+      }
+    },
     components: {
       JokeBlock
     },
